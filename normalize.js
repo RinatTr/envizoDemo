1f6fd13d2fb7d1ea10d7e0e79c6059674911d319
RinatTr <rinatreg@gmail.com>
2019-04-15 15:50:01 -0400

    connect .json to script

---
+++ normalize.js
@@ -0,0 +1,58 @@
+const normalizeData = (info) => {
+
+  let slicedInfo = info.filter(el => {return el.month.includes("2018") || el.month.includes("2019")})
+  //normalize
+  let data = {
+      bronx: [],
+      queens: [],
+      manhattan: [],
+      brooklyn: [],
+      staten_island: []
+    };
+    //divide objects per boroughs
+    slicedInfo.forEach(entry => {
+      switch (entry.borough) {
+        case "Bronx":
+          data.bronx.push(entry);
+          break;
+        case "Manhattan":
+          data.manhattan.push(entry);
+          break;
+        case "Queens":
+          data.queens.push(entry);
+          break;
+        case "Brooklyn":
+          data.brooklyn.push(entry);
+          break;
+        case "Staten Island":
+          data.staten_island.push(entry);
+          break;
+      }
+    });
+    //sum all districts and organize in obj
+    let summaries = {
+      bronx: {},
+      queens: {},
+      manhattan: {},
+      brooklyn: {},
+      staten_island: {}
+    };
+
+    for (let borough in data) {
+      data[borough].forEach(el => {
+        !summaries[borough][el.month]
+          ? (summaries[borough][el.month] = [el])
+          : summaries[borough][el.month].push(el);
+      });
+
+      for (let month in summaries[borough]) {
+        summaries[borough][month] =
+          summaries[borough][month].length > 1
+            ? summaries[borough][month].reduce((acc, el) => {
+                return acc + parseFloat(el.refusetonscollected);
+              }, 0)
+            : parseFloat(summaries[borough][month][0].refusetonscollected);
+      }
+    }
+  return [summaries];
+}
